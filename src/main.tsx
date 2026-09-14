3e129831cfcdef51864241a296cb657c2d9c02a3
edmundomarques98-bit <edmundomarques98@gmail.com>
2026-09-14 08:37:14 -0300

    feat: ativar fotos reais nos cards de produtos

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import ScrollStory from './ScrollStory';
+import ProductPhotos from './ProductPhotos';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
+        <ProductPhotos />
         <ScrollStory />
     </StrictMode>
 );
